a6493f17a3f6f401e2145620638793363a9a4ab6
Deyoungj <emmanuelonyibor39@gmail.com>
2022-10-30 19:49:56 +0100

    content

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,33 @@
 import "./App.css";
-import Sidebar from './components/Sidebar';
-import TopHeader from './components/Header'
-import { Layout, Breadcrumb} from "antd";
-
-
-
+import Sidebar from "./components/Sidebar";
+import TopHeader from "./components/Header";
+import { Layout, Breadcrumb } from "antd";
 
 function App() {
   return (
     <Layout>
       <TopHeader />
       <Layout>
-          <Sidebar />
-          <Layout>
-            hellow
-          </Layout>
+        <Sidebar />
+        <Layout style={{ padding: "0 24px 24px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
       </Layout>
     </Layout>
-    
-      
-      
-
-      
   );
 }
 
